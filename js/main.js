f17fad6040c91305de609f59a72f87e18f176f31
alex baboy <alexbaboy2016@gmail.com>
2021-10-08 10:53:21 +0300

    menu started

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
-const swiper = new Swiper('.slider-container', {
+const sliderContainer = document.querySelector('.slider-container'),
+			burger = document.querySelector('.burger'),
+			close = document.querySelector('.menu__close'),
+			menu = document.querySelector('.menu')
+
+const swiper = new Swiper(sliderContainer, {
 	centeredSlides: true,
 	slidesPerView: 'auto',
 	loop: true,
 	spaceBetween: 100
+})
+
+burger.addEventListener('click', () => {
+	menu.classList.add('menu__visible')
 })
