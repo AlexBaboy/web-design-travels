345db3cde3c82afda9d1b0c8465d2d0b57c2b103
alex baboy <alexbaboy2016@gmail.com>
2021-10-15 00:08:02 +0300

    first slider

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@ const sliderContainer = document.querySelector('.slider-container'),
 			burger = document.querySelector('.burger'),
 			close = document.querySelector('.menu__close'),
 			menu = document.querySelector('.menu'),
-			swiperContainer = document.querySelector('.swiper-container')
+			swiperContainer = document.querySelector('.swiper-container'),
+			playButton = document.querySelectorAll('.main-slider__play');
 
 const slider = new Swiper(sliderContainer, {
 	centeredSlides: true,
@@ -19,15 +20,39 @@ const swiper = new Swiper(swiperContainer, {
 	fadeEffect: {
 		crossFade: true
 	},
-	effect: 'fade'
+	effect: 'fade',
+	navigation: {
+		nextEl: '.btn-right',
+		prevEl: 'btn-left'
+	}
+})
+
+swiper.on('transitionEnd', function() {
+	const videos = document.querySelectorAll('.first__slider video');
+	videos.forEach(el => {
+		el.pause();
+		el.currentTime = 0;
+	})
+	playButton.forEach(el => {
+		el.style.display = 'block'
+	})
 })
 
 burger.addEventListener('click', () => {
 	menu.classList.add('menu__visible')
-	console.log('15!')
 })
 
 close.addEventListener('click', () => {
 	menu.classList.remove('menu__visible')
-	console.log('20!')
+})
+
+playButton.forEach(el => {
+	el.addEventListener('click', e => {
+		const video = e.currentTarget.closest('.main-slider__media').querySelector('video');
+		video.play();
+		e.currentTarget.style.display = 'none';
+		setTimeout(()=> {
+			video.volume = 1
+		}, 1000)
+	})
 })
