3df12923be257f9e5de64db9be9eb6d66bce98e2
alex baboy <alexbaboy2016@gmail.com>
2021-10-14 10:51:45 +0300

    menu fixed

--- js/main.js
+++ js/main.js
@@ -1,13 +1,25 @@
 const sliderContainer = document.querySelector('.slider-container'),
 			burger = document.querySelector('.burger'),
 			close = document.querySelector('.menu__close'),
-			menu = document.querySelector('.menu')
+			menu = document.querySelector('.menu'),
+			swiperContainer = document.querySelector('.swiper-container')
 
-const swiper = new Swiper(sliderContainer, {
+const slider = new Swiper(sliderContainer, {
 	centeredSlides: true,
 	slidesPerView: 'auto',
 	loop: true,
 	spaceBetween: 100
+})
+
+const swiper = new Swiper(swiperContainer, {
+	centeredSlides: true,
+	slidesPerView: 1,
+	loop: true,
+	spaceBetween: 10,
+	fadeEffect: {
+		crossFade: true
+	},
+	effect: 'fade'
 })
 
 burger.addEventListener('click', () => {
